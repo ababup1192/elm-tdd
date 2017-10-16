ee8d5642377ace0f2f5db56febeabd35be4ecf49
abab <ababup1192@gmail.com>
2017-10-16 23:16:41 +0900

    Modify test to tell the intention

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -12,18 +12,10 @@ import Dollar exposing (..)
 all : Test
 all =
     describe "Money Test"
-        [ "Multiplication1"
-            => let
-                ten =
-                    Dollar 5 |> Dollar.times 2
-               in
-                ten
-                    === Dollar 10
-        , "Multiplication2"
-            => let
-                fifteen =
-                    Dollar 5 |> Dollar.times 3
-               in
-                fifteen
-                    === Dollar 15
+        [ "equivalence"
+            => Dollar 5
+            === Dollar 5
+        , "Multiplication"
+            => (Dollar 5 |> Dollar.times 3)
+            === Dollar 15
         ]
