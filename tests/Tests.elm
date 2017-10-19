bb6e769f99970b52f0e9f5fd8eed3abd1e215cff
abab <ababup1192@gmail.com>
2017-10-19 22:21:34 +0900

    Add Franc Test

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -12,10 +12,20 @@ import Dollar exposing (..)
 all : Test
 all =
     describe "Money Test"
-        [ "equivalence"
-            => Dollar 5
-            === Dollar 5
-        , "Multiplication"
-            => (Dollar 5 |> Dollar.times 3)
-            === Dollar 15
+        [ describe "Dollar"
+            [ "Multiplication1"
+                => (Dollar 5 |> Dollar.times 2)
+                === Dollar 10
+            , "Multiplication2"
+                => (Dollar 5 |> Dollar.times 3)
+                === Dollar 15
+            ]
+        , describe "Franc"
+            [ "Multiplication1"
+                => (Franc 5 |> Franc.times 2)
+                === Franc 10
+            , "Multiplication2"
+                => (Franc 5 |> Franc.times 3)
+                === Franc 15
+            ]
         ]
