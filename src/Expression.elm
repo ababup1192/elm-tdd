755f8a0743c1f488b6ccb7a1c11256448809ecae
abab <ababup1192@gmail.com>
2017-11-11 19:25:20 +0900

    Move Expression functions

--- src/Expression.elm
+++ src/Expression.elm
@@ -1,8 +1,47 @@
-module Expression exposing (..)
+module Expression exposing (Expression(..), ($+), ($*), currency, amount)
 
-import Money.Model exposing (Money)
+import Money.Model exposing (Money(..), Currency(..))
 
 
 type Expression
     = Single Money
     | Sum Expression Expression
+
+
+($+) : Expression -> Expression -> Expression
+($+) exp1 exp2 =
+    Sum exp1 exp2
+
+
+($*) : Expression -> Int -> Expression
+($*) exp multiplier =
+    case exp of
+        Single (Money amnt crncy) ->
+            Single <| Money (amnt * multiplier) crncy
+
+        Sum exp1 exp2 ->
+            let
+                mlp_ e =
+                    e $* multiplier
+            in
+                Sum (mlp_ exp1) (mlp_ exp2)
+
+
+currency : Expression -> Currency
+currency expression =
+    case expression of
+        Single (Money _ currency) ->
+            currency
+
+        Sum _ exp2 ->
+            currency exp2
+
+
+amount : Expression -> Int
+amount expression =
+    case expression of
+        Single (Money amnt _) ->
+            amnt
+
+        Sum exp1 exp2 ->
+            (amount exp1) + (amount exp2)
